b6ac788ddfd96e98ed5d0ae8322a03d68fa3ba6e
Javi <jrofe96@gmail.com>
2024-01-24 14:36:10 +0100

    relacion3 ej4

--- Relacion3/ej4/formulario.php
+++ Relacion3/ej4/formulario.php
@@ -1,4 +1,26 @@
 <?php
-// Escribe un programa que sume, reste, multiplique y divida dos números introducidos por un
-// formulario.
+// Escribe un programa que sume, reste, multiplique y divida dos números introducidos por un formulario.
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <form action="resultado.php" method="get">
+        Número 1:
+        <input type="number" name="num1"><br>
+        <label for="operador">Operador</label>
+            <select name="operadores" id="operador">
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">*</option>
+                <option value="/">/</option>
+            </select><br>
+        Número 2:
+            <input type="number" name="num2"><br>
+            <input type="submit" value="Enviar">
+        </form>
+
+    </body>
+</html>
--- Relacion3/ej4/resultado.php
+++ Relacion3/ej4/resultado.php
@@ -1,4 +1,34 @@
-<?php
-
-
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <?php
+            if (isset($_GET["num1"]) and isset($_GET["num2"]) and isset($_GET["operador"])) {
+                if (is_numeric($_GET["num1"]) and is_numeric($_GET["num2"])) {
+                    $num1=$_GET["num1"];
+                    $num2=$_GET["num2"];
+                    $operador=$_GET["operador"];
+                    
+                    if ($operador == "+") {
+                        echo $resultado = $num1 + $num2;
+                    } elseif ($operador == "-") {
+                        echo $resultado = $num1 - $num2;
+                    } elseif ($operador == "*"){
+                        echo $resultado = $num1 * $num2;
+                    } elseif ($operador == "/"){
+                        echo $resultado = $num1 / $num2;
+                    } else {
+                        echo "error";
+                    }
+                
+                }
+            } else {
+                echo "Error";
+            }
+            
+        ?>
+        <br><a href="formulario.php">Volver a la calculadora</a>
+    </body>
+</html>
