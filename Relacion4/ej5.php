9779196957445a020a9748d9ad0cb329b6a1cd27
Javi <jrofe96@gmail.com>
2024-01-25 14:06:57 +0100

    relacion4 done

--- Relacion4/ej5.php
+++ Relacion4/ej5.php
@@ -1,4 +1,6 @@
 <?php
-// Usando la librería del ejercicio 4. Muestra el número mínimo de un array de enteros
-// de tamaño 10 generado aleatoriamente.
+// Usando la librería del ejercicio 4. Muestra el número mínimo de un array de enteros de tamaño 10 generado aleatoriamente.
+include 'ej4_lib.php';
+$array=generaArrayInt(10);
+echo "El mínimo de la array es: ".minimoArrayInt($array)."\n"
 ?>
--- Relacion4/ej6.php
+++ Relacion4/ej6.php
@@ -1,4 +1,6 @@
 <?php
-// Usando la librería del ejercicio 4. Muestra el número máximo y mínimo de un array
-// de enteros de tamaño 10 generado aleatoriamente.
+// Usando la librería del ejercicio 4. Muestra el número máximo y mínimo de un array de enteros de tamaño 10 generado aleatoriamente.
+include 'ej4_lib.php';
+$array=generaArrayInt(10);
+echo "El máximo de la array es: ".maximoArrayInt($array)." y el mínimo es: ".minimoArrayInt($array)."\n";
 ?>
