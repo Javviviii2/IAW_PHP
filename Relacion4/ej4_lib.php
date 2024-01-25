c39e9681c49e91facb5a817b17f06ddc7f44ddb1
Javi <jrofe96@gmail.com>
2024-01-25 13:54:15 +0100

    ej4_lib

--- Relacion4/ej4_lib.php
+++ Relacion4/ej4_lib.php
@@ -4,6 +4,17 @@
 // b. minimoArrayInt: Devuelve el mínimo del array que se pasa como parámetro.
 // c. maximoArrayInt: Devuelve el máximo del array que se pasa como parámetro.
 
-
-
+function generaArrayInt($array_range){
+    $array=array();
+    for ($i=0; $i <$array_range;$i++) { 
+        array_push($array,random_int(0,100));
+    }
+    return $array;
+}
+function minimoArrayInt($array){
+    return min($array);
+}
+function maximoArrayInt($array){
+    return max($array);
+}
 ?>
