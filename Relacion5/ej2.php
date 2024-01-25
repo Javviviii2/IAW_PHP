7b7381c260cae24907ba95a4fbcdd59c2afd0b72
Javi <jrofe96@gmail.com>
2024-01-25 14:08:41 +0100

    reorganizando relacion5

--- Relacion5/ej2.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Escriba un programa que conste de dos páginas y que solicite una edad.
-//  ● En la primera página se solicita una edad entre 18 y 130 años.
-//  ● La segunda página comprueba si se ha escrito un número entero entre 18 y 130.
-//      - Si se ha escrito un número entero entre 18 y 130, lo muestra.
-//      - Si la edad se deja vacía, si no se escribe un número o si se escribe un
-//        número que no esté comprendido entre 18 y 130, vuelve automáticamente a
-//        la primera página, mostrando el aviso correspondiente.
-?>
